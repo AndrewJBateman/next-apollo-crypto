4ed7c2a59079efdd9dae5cc836c4b86d7e92990d
AndrewJBateman <gomezbateman@yahoo.com>
2021-06-14 17:54:47 +0200

    chore: linting of code

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,23 @@
-import App from 'next/app'
-import { ApolloProvider } from '@apollo/react-hooks'
+import App from 'next/app';
+import { ApolloProvider } from '@apollo/react-hooks';
 import Container from '@material-ui/core/Container';
 import withData from '../utils/apollo-client';
 import Layout from '../components/_App/Layout';
 
 class MyApp extends App {
-  render() {
-    const { Component, apollo } = this.props
+	render() {
+		const { Component, apollo } = this.props;
 
-    return (
-      <ApolloProvider client={apollo}>
-        <Layout>
-          <Container>
-            <Component />
-          </Container>
-        </Layout>
-      </ApolloProvider>
-    )
-  }
+		return (
+			<ApolloProvider client={apollo}>
+				<Layout>
+					<Container>
+						<Component />
+					</Container>
+				</Layout>
+			</ApolloProvider>
+		);
+	}
 }
 
-export default withData(MyApp)
+export default withData(MyApp);
--- pages/api/graphql/index.js
+++ pages/api/graphql/index.js
@@ -1,18 +1,20 @@
-import { ApolloServer } from 'apollo-server-micro'
-import resolvers from './resolvers'
-import typeDefs from './typeDefs'
+// import type definitions & resolvers then create new Apollo server and define path
+
+import { ApolloServer } from 'apollo-server-micro';
+import resolvers from './resolvers';
+import typeDefs from './typeDefs';
 
 const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers
-})
+	typeDefs,
+	resolvers,
+});
 
-const server = apolloServer.createHandler({ path: '/api/graphql' })
+const server = apolloServer.createHandler({ path: '/api/graphql' });
 
 export const config = {
-  api: {
-    bodyParser: false
-  }
-}
+	api: {
+		bodyParser: false,
+	},
+};
 
-export default server
+export default server;
