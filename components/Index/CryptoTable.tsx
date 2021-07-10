e489fe6309b2114269f9dd3ecd3212430c6115cf
AndrewJBateman <gomezbateman@yahoo.com>
2021-07-10 20:38:46 +0200

    feat: crypto detail page added with link

--- components/Index/CryptoTable.tsx
+++ components/Index/CryptoTable.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
 
-import { formatCurrency } from '../../utils/filters';
+import { formatCurrency } from "../../utils/filters";
 
 const useStyles = makeStyles({
 	table: {
@@ -29,7 +30,7 @@ const CryptoTable = ({ cryptos }) => {
 					<TableHead>
 						<TableRow>
 							<TableCell>Rank</TableCell>
-              <TableCell>Logo</TableCell>
+							<TableCell>Logo</TableCell>
 							<TableCell align="left">Name</TableCell>
 							<TableCell align="left">Symbol</TableCell>
 							<TableCell align="right">Price</TableCell>
@@ -37,21 +38,19 @@ const CryptoTable = ({ cryptos }) => {
 					</TableHead>
 					<TableBody>
 						{cryptos.map((row) => (
-							<TableRow key={row.id}>
+              <Link href="/crypto/[id]" as={`/crypto/${row.id}`}>
+							<TableRow key={row.id} onClick={() => console.log("clicked")}>
 								<TableCell component="th" scope="row">
 									{row.rank}
 								</TableCell>
-                <TableCell>
-									<Avatar alt="image of crypto" src={row.logo_url} className={classes.small} />
+								<TableCell>
+									<Avatar alt="image of crypto" src={row.logo_url} />
 								</TableCell>
-								<TableCell align="left">
-									{row.name}
-								</TableCell>
-                <TableCell align="left">
-                  {row.currency}
-								</TableCell>
+								<TableCell align="left">{row.name}</TableCell>
+								<TableCell align="left">{row.currency}</TableCell>
 								<TableCell align="right">{formatCurrency(row.price)}</TableCell>
 							</TableRow>
+              </Link>
 						))}
 					</TableBody>
 				</Table>
---
+++ pages/crypto/[id].tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+export default function Crypto({ crypto }) {
+  return (
+
+    <Head>
+      <title>Hello</title>
+    </Head>
+  )
+}
